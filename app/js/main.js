e6098498e612f44a12d0cc09674b7ff44d059be6
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-08-20 12:27:59 +0400

    feat: other position

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 const selects = document.querySelectorAll('.select');
+const positions = document.querySelectorAll('.position-item');
+const choosenPosition = document.querySelector('#choosen-position');
+const other = document.querySelector('#other');
+const otherPosition = document.querySelector('#other-position-container');
 
-
+// toggle dropdown
 selects.forEach(element => {
     element.addEventListener("click", () => {
         if (!element.classList.contains("open-select")) {
@@ -13,4 +17,23 @@ selects.forEach(element => {
             element.classList.add('close-dropdown');
         }
     });
+});
+
+// choose position
+positions.forEach(element => {
+    element.addEventListener('click', () => {
+        const value = element.textContent;
+        choosenPosition.innerText = value;
+        choosenPosition.classList.add("choosen");
+
+        otherPosition.classList.add("hide");
+    });
+});
+
+other.addEventListener("click", () => {
+    choosenPosition.classList.remove("choosen");
+    choosenPosition.innerText = other.textContent;
+    otherPosition.classList.remove("hide");
+    const el = document.querySelector('#other-position');
+    el.focus();
 });
